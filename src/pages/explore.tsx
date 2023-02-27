53905d22e2c2c80f42a57054a1079b73b1195493
Shlomi Nugarker <shlomin1231@gmail.com>
2023-02-27 20:29:32 +0200

    Explore page

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -3,7 +3,21 @@ import Head from 'next/head'
 
 import styles from '../styles/pages/_explore.module.scss'
 
+import { CiSearch } from 'react-icons/ci'
+import MovieList from '@/cmps/MovieList'
+import { useState } from 'react'
+
 export default function Explore() {
+  const [movies, setMovies] = useState<any>()
+
+  const loadMovies = async () => {
+    const movies = await import('../data/top-rated.json')
+    console.log(movies)
+    setMovies(movies.results)
+  }
+
+  loadMovies()
+
   return (
     <>
       <Head>
@@ -14,6 +28,22 @@ export default function Explore() {
       </Head>
       <main className={styles.explore}>
         <SideNav />
+
+        <div className={styles.container}>
+          {/* INPUT */}
+          <div className={styles['search-container']}>
+            <div>
+              <span>
+                <CiSearch />
+              </span>
+              <input type="text" placeholder="Movies, shows and more" />
+            </div>
+          </div>
+
+          {/* LIST */}
+          <p className={styles.title}>Popular Searches</p>
+          <MovieList movies={movies} />
+        </div>
       </main>
     </>
   )
