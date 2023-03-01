ab7e51d2bba3f4db30597f5b32e37d78a1543757
Shlomi Nugarker <shlomin1231@gmail.com>
2023-03-02 01:30:45 +0200

    youTubeService

--- src/services/frontend/youtubeService.ts
+++ src/services/frontend/youtubeService.ts
@@ -1,35 +1,33 @@
-import axios from 'axios'
-import { storageService } from './storageService'
+import { storageService } from '../frontend/storageService'
+
+const BASE_URL =
+  process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3000/api/'
 
 export const youTubeService = {
   getVideos,
 }
 
-getVideos()
-
-async function getVideos(term: string = 'Dilwale Dulhania Le Jayenge') {
+async function getVideos(term: string) {
   try {
     const termVideosMap = storageService.load('videosDB') || {}
     if (termVideosMap[term]) {
       return Promise.resolve(termVideosMap[term])
     }
 
-    const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API
-    const res = await axios.get(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${apiKey}&q=${term} Official trailer`
-    )
-
-    const videos = res.data.items.map((video: any) => ({
-      id: video.id.videoId,
-      title: video.snippet.title,
-      img: {
-        url: video.snippet.thumbnails.default.url,
-        width: video.snippet.thumbnails.default.width,
-        height: video.snippet.thumbnails.default.height,
+    const res = await fetch(`${BASE_URL}/videos`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
-    }))
+      body: JSON.stringify({ term }),
+    })
+
+    const videos = await res.json()
+
     termVideosMap[term] = videos
     storageService.store('videosDB', termVideosMap)
+
     return videos
   } catch (err) {
     console.log(err)
